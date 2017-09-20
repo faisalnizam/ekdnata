2bb4e059afa5b6289941be5e7ffa3ae3d97000a1
Faisal Nizam <faisal.nizam@careem.com>
2017-09-20 13:08:14 +0400

    Adding Graphite

--- setupenv.sh
+++ setupenv.sh
@@ -3,4 +3,23 @@
 
 echo "Setup Environment" 
 
+# Using source func/help.py
 
+
+DR=/usr/local/bin/docker 
+PY=/usr/bin/python 
+
+
+
+while getopts 'p:r:n:u:h' OPT; do
+  case $OPT in
+    p)  profile=$OPTARG;; 
+    n)  env_name=$OPTARG;;
+    h)  ${PY} `func/help.py` 
+	exit 
+	;;
+  esac
+done
+
+profile=${profile:=unknown} 
+env_name=${env_name:=unknown}
